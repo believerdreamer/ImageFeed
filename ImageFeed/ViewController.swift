2e4f047642bd60412fd7ab616edb8f5289c2bb87
Архип Семёнов <liteplay03@ya.ru>
2023-07-28 15:15:23 +0300

    Added .gitignore

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Архип Семёнов on 28.07.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
