1616da75cc1c1d89580239b6faeb6e0f0bd327f1
Архип Семёнов <liteplay03@ya.ru>
2023-07-28 19:02:41 +0300

    Added launch screen with YP logo

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -9,7 +9,6 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    print("hello, sourcetree!")
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
