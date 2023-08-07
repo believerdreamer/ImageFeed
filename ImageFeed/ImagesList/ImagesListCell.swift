553c3d53c421f44c7cf6a6ddda540513760bb731
Архип Семёнов <liteplay03@ya.ru>
2023-08-07 21:07:26 +0300

    added custom cell, added custom cell method, added constrains

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIndetifier = "ImagesListCell"
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var cellImage: UIImageView!
+}
+
