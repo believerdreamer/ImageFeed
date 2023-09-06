f2d55766d47378bd58cbf2e7362e27394e06030c
Архип Семёнов <liteplay03@ya.ru>
2023-09-06 20:36:26 +0300

    Изменена иерархия файлов, изменен способ показа картинки по тапу в ленте: теперь картинка раскрывается на весь экран. Добавлена кнопка возврата в ленту изображений из экрана показа картинки. Исправлена кнопка выхода из профиля на экране профиля.

--- ImageFeed/View Controllers/SingleImageViewController.swift
+++ ImageFeed/View Controllers/SingleImageViewController.swift
@@ -3,6 +3,10 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage!
     
+    @IBAction func clickBackButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
     @IBOutlet var imageView: UIImageView! {
         didSet {
             guard isViewLoaded else { return }
