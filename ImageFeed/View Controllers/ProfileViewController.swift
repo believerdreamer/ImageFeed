c2fbc089c885480fd8c5a4776ea6159459035edf
Arkhip Semenov <liteplay03@ya.ru>
2023-09-16 12:33:35 +0300

    Страница профиля (ProfileViewController.swift) была свёрстана в IB, теперь верстка полностью осуществлена кодом. Улучшено качество аватарки профиля и кнопки выхода.

--- ImageFeed/View Controllers/ProfileViewController.swift
+++ ImageFeed/View Controllers/ProfileViewController.swift
@@ -1,11 +1,84 @@
 import UIKit
 
-final class ProfileViewContoller: UIViewController{//MARK: UIViewController
-    //MARK: @IBoutlet
+final class ProfileViewContoller: UIViewController{ //MARK: UIViewController
+    
+    @objc func didTapButton() {
+        
+    }
     
     //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureProfileImage()
+        configureExitButton()
+        configureNameLabel()
+        configureNickname()
+        configureDescription()
         
     }
+    
+    func configureProfileImage() {
+    
+        let profileImage = UIImage(named: "UserPhoto")
+        let imageView = UIImageView(image: profileImage)
+        imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+    }
+    
+    func configureExitButton() {
+        let button = UIButton.systemButton(
+            with: UIImage(systemName: "ipad.and.arrow.forward")!,
+            target: self,
+            action: #selector(self.didTapButton))
+        
+        button.tintColor = UIColor(named: "YPRed")
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+        button.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 45).isActive = true
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+    }
+    
+    func configureNameLabel() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.text = "Екатерина Новикова"
+        label.font = UIFont.boldSystemFont(ofSize: 23)
+        label.textColor = .white
+        label.widthAnchor.constraint(equalToConstant: 241).isActive = true
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 110).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        
+        
+    }
+    
+    func configureNickname() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.text = "@ekaterina_nov"
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = UIColor(named: "YPGrey")
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 144).isActive = true
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+    }
+    
+    func configureDescription() {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .white
+        label.text = "Hello, world!"
+        label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 170).isActive = true
+    }
+
 }
